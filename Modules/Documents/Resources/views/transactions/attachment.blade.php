0a2087b7f7cf5c507f8e39c90728ed58af8a31c6
ferdie <ferdiebergado@gmail.com>
2018-03-27 21:37:16 +0800

    Fixed remove attachment placement.

--- Modules/Documents/Resources/views/transactions/attachment.blade.php
+++ Modules/Documents/Resources/views/transactions/attachment.blade.php
@@ -6,7 +6,7 @@
 			<div class="form-group">
 				<label id="lblAttachment" for="attachment">Attachment(s):</label>
 				<p id="ajax-loader" style="display: none;">
-					Uploading... 
+					Processing... 
 					<img src="{{ url('/storage') . '/' . 'ajax-loader-square.gif' }}">				
 				</p>
 				<div id="divFiles" class="input-group">					
@@ -21,74 +21,55 @@
 </div>
 @push('scripts')
 <script>
+	var loader = $('#ajax-loader');
 	var removeAttachment = function (elementId, filepath) {
-        // Removes an element from the document
-        // var element = document.getElementById(elementId);
-        // element.parentNode.removeChild(element);
-        $('#uploaded' + elementId).remove();
-        $.post('{{ route('attachments.delete') }}', {'path': filepath}, function(data, textStatus, jqXHR) {
-        	return data;
-        });
-    }    	
-    $(function() {
-    	var i = 1;
-    // var fileId = 0; // used by the addFile() function to keep track of IDs
-    // function addElement(parentId, elementTag, elementId, html) {
-    //     // Adds an element to the document
-    //     var p = document.getElementById(parentId);
-    //     var newElement = document.createElement(elementTag);
-    //     newElement.setAttribute('id', elementId);
-    //     newElement.innerHTML = html;
-    //     p.appendChild(newElement);
-    // }
-    // function removeElement(elementId) {
-    //     // Removes an element from the document
-    //     var element = document.getElementById(elementId);
-    //     element.parentNode.removeChild(element);
-    // }    
-
-    // function addFile() {
-    //     fileId++; // increment fileId to get a unique ID for the new element
-    //     var html = '<input type="file" name="uploaded_files[]" /> ' +
-    //     '<a href="" onclick="javascript:removeElement('file-' + fileId + ''); return false;">Remove</a>';
-    //     addElement('files', 'p', 'file-' + fileId, html);
-    // }		
-    $('#ajax-loader').hide();
-    $("#attachment-form").submit(function(evt){	 
-    	evt.preventDefault();
-    	var formData = new FormData($(this)[0]);
-    	var foruploaded = $('#attachment').val();
-    	var file = foruploaded.split("\\");
-    	if (file.length) uploadedfile = file[file.length-1];   	
-    	$('#ajax-loader').show();
-    	var errorEl = $('#errorAttachment');
-    	if (errorEl) {
-    		errorEl.remove();
-    	}			
-    	$.ajax({
-    		url: '{{ route('attachments.store') }}',
-    		type: 'POST',
-    		data: formData,
-    		async: true,
-    		cache: false,
-    		contentType: false,
-    		enctype: 'multipart/form-data',
-    		processData: false,
-    		success: function (response) {
-    			var uploaded = `<p><a href=\"${response.fileurl}\" target=\"_blank\">${uploadedfile}</a> &nbsp;&nbsp; <a id=\"uploaded${i}\" class=\"text-danger\" onclick=\"removeAttachment(${i},\'` + response.filepath + `\'); return false;\"><i class=\"fa fa-times\" title=\"Remove\"></i></a></p>`;
-    			$('#ajax-loader').hide();
-    			if (response.message.attachment) {
-    				var message = `<p id=\"errorAttachment\" class=\"text-danger\"> ${response.message.attachment} </p>`;
-    				$('#lblAttachment').after(message);
-    			} else {
-    				$('#lblAttachment').after(uploaded);						
-    			}
-    			$('#attachment').val('');
-    			i++;
-    		}
-    	});
-    	return false;
-    }); 
-});
+		var elm = $("#divFiles");
+		loader.show();
+		$('#uploaded' + elementId).replaceWith(loader);
+		$('#uploadRemove' + elementId).replaceWith(elm);
+		$.post('{{ route('attachments.delete') }}', {'path': filepath}, function(data, textStatus, jqXHR) {
+			loader.hide();
+			return data;
+		});
+	}    	
+	$(function() {
+		var i = 1;	
+		loader.hide();
+		$("#attachment-form").submit(function(evt){	 
+			evt.preventDefault();
+			var formData = new FormData($(this)[0]);
+			var foruploaded = $('#attachment').val();
+			var file = foruploaded.split("\\");
+			if (file.length) uploadedfile = file[file.length-1];   	
+			loader.show();
+			var errorEl = $('#errorAttachment');
+			if (errorEl) {
+				errorEl.remove();
+			}			
+			$.ajax({
+				url: '{{ route('attachments.store') }}',
+				type: 'POST',
+				data: formData,
+				async: true,
+				cache: false,
+				contentType: false,
+				enctype: 'multipart/form-data',
+				processData: false,
+				success: function (response) {
+					var uploaded = `<p><a id=\"uploaded${i}\" href=\"${response.fileurl}\" target=\"_blank\">${uploadedfile}</a> &nbsp;&nbsp; <a id=\"uploadRemove${i}\" class=\"text-danger\" onclick=\"removeAttachment(${i},\'` + response.filepath + `\'); return false;\"><i class=\"fa fa-times\" title=\"Remove\"></i></a></p>`;
+					loader.hide();
+					if (response.message.attachment) {
+						var message = `<p id=\"errorAttachment\" class=\"text-danger\"> ${response.message.attachment} </p>`;
+						$('#lblAttachment').after(message);
+					} else {
+						$('#lblAttachment').after(uploaded);						
+					}
+					$('#attachment').val('');
+					i++;
+				}
+			});
+			return false;
+		}); 
+	});
 </script>	
 @endpush
